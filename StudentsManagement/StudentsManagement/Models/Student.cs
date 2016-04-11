6e3e5a77ee9f9bff5b6d4fa1a789a0a2b893891e
alexandrinaFilimonov <alexandrina.filimonov@gmail.com>
2016-04-11 22:11:28 +0300

    Added Student model

---
+++ StudentsManagement/StudentsManagement/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentsManagement.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Cnp { get; set; }
+        public string Sid { get; set; }
+    }
+}
